f01ea77257ee5a4bb52a3d4416a73a04b1894bce
pc <pc@NOUHAILA>
2021-10-12 21:57:38 +0200

    Ajoutez des fichiers projet.

---
+++ exercice01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercice1
+{
+    class Program
+    {
+        public static void premier(int number)
+        {
+            int i = 2;
+            while ((number % i != 0) && (i < Math.Sqrt(number)))
+            {
+                i++;
+            }
+            if (number % i == 0)
+                Console.WriteLine("Ce nombre est premier!");
+            else
+                Console.WriteLine("Ce nombre n'est pas premier!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enterz un nombre!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            premier(number);
+        }
+    }
+}
